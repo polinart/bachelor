3d592c271a87ec3f22b319bb718cf4d87acb106b
polina <polinart21@gmail.com>
2016-05-07 18:08:27 +0300

    gitignore dist

---
+++ app/scripts/controllers/chatController.js
@@ -0,0 +1,46 @@
+angular.module('sbAdminApp').controller('Shell', Shell);
+
+function Shell() {
+
+    var vm = this;
+
+    vm.messages = [
+        {
+            'username': 'username1',
+            'content': 'Hi!'
+        },
+        {
+            'username': 'username2',
+            'content': 'Hello!'
+        },
+        {
+            'username': 'username2',
+            'content': 'Hello!'
+        },
+        {
+            'username': 'username2',
+            'content': 'Hello!'
+        },
+        {
+            'username': 'username2',
+            'content': 'Hello!'
+        },
+        {
+            'username': 'username2',
+            'content': 'Hello!'
+        }
+    ];
+
+    vm.username = 'username1';
+
+    vm.sendMessage = function(message, username) {
+        if(message && message !== '' && username) {
+            vm.messages.push({
+                'username': username,
+                'content': message
+            });
+        }
+    };
+    vm.visible = true;
+    vm.expandOnNew = true;
+}
