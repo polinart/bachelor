81cce43c53faa6f39f608dc94ebb5248942a3d42
polina <polinart21@gmail.com>
2016-05-22 17:51:04 +0300

    test sidemenu items

--- acceptance-tests/tests/acceptance/SidebarCest.php
+++ acceptance-tests/tests/acceptance/SidebarCest.php
@@ -12,7 +12,120 @@ class SidebarCest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function testToOpenPersonalDataMenuItem(AcceptanceTester $I)
     {
+        $I->wantTo('open user profile page');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/a');
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/ul/li[1]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/ul/li[1]/a');
+        $I->wait(1);
+        $I->seeElement('img.avatar');
+        $I->seeElement('div.card');
+    }
+
+    public function testToOpenContracts(AcceptanceTester $I) {
+        $I->wantTo('open contracts page');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/a');
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/ul/li[2]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/ul/li[2]/a');
+        $I->wait(1);
+        $I->see('Contracts');
+    }
+
+    public function testToOpenPayments(AcceptanceTester $I){
+        $I->wantTo('open payments page');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/a');
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/ul/li[3]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/ul/li[3]/a');
+        $I->wait(1);
+        $I->see('Payments and bills');
+    }
+
+    public function testToOpenRectorsOrder(AcceptanceTester $I) {
+        $I->wantTo('open rectors orders page');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/a');
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/ul/li[4]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/ul/li[4]/a');
+        $I->wait(1);
+        $I->see('Rector\'s orders');
+    }
+
+    public function testToOpenGrades(AcceptanceTester $I) {
+        $I->wantTo('open grades page');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/a');
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[3]/ul/li[5]/a');
+        $I->click('//*[@id="side-menu"]/li[3]/ul/li[5]/a');
+        $I->wait(1);
+        $I->see('Person grades');
+    }
+
+    public function testToOpenDocuments(AcceptanceTester $I) {
+        $I->wantTo('open documents page');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[8]/a');
+        $I->click('//*[@id="side-menu"]/li[8]/a');
+        $I->wait(1);
+        $I->see('Documents and Statements templates');
+    }
+
+    public function testToOpenFAQ(AcceptanceTester $I) {
+        $I->wantTo('open FAQ page');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('//*[@id="side-menu"]/li[9]/a');
+        $I->click('//*[@id="side-menu"]/li[9]/a');
+        $I->wait(1);
+        $I->see('Frequently Asked Questions');
+    }
+
+    public function testThatPublicSiteMenuItemContainsCorrectLink(AcceptanceTester $I) {
+        $I->wantTo('check public site menu item contains correct link');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('a[href="http://tsi.lv"]');
+    }
+
+    public function testThatElearningMenuItemCntainesCorrectLink(AcceptanceTester $I) {
+        $I->wantTo('check e-learning system menu item contains correct link');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('a[href="http://e.tsi.lv"]');
+    }
+
+    public function testThatLibraryMenuItenContainesCorrectLink(AcceptanceTester $I) {
+        $I->wantTo('check library menu item contains correct link');
+        $I->amOnPage('index.html#/dashboard/home');
+        $I->reloadPage();
+        $I->wait(1);
+        $I->seeElement('a[href="http://services.tsi.lv/library/page.php"]');
     }
 }
