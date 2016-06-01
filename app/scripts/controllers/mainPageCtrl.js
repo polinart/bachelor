0ac7acb5eb40a540e33b328669249be45554ddc4
polina <polinart21@gmail.com>
2016-06-01 11:33:27 +0300

    main page method

--- app/scripts/controllers/mainPageCtrl.js
+++ app/scripts/controllers/mainPageCtrl.js
@@ -4,4 +4,14 @@
  */
 angular.module('sbAdminApp')
   .controller('MainCtrl', function($scope,$position) {
+        var url = 'dataStore/mainPage.json';
+
+        getNews(url, $scope, $http);
   });
+
+function getNews (url, $scope, $http) {
+    $http.get(url).success(function (data) {
+
+        $scope.news = data;
+    });
+}
